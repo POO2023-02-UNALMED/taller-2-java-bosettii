337560d9f013932c76931835b231fd63094adbe0
lanard leopold <80700251+bosettii@users.noreply.github.com>
2023-08-20 12:56:06 -0500

    Add files via upload

---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,14 @@
+package test;
+public class Motor{
+    int numeroCilindors;
+    String tipo;
+    int registro;
+
+    void cambiarRegistro(int a){
+        this.registro = a;
+    }
+    
+    void asignarTipo(String t){
+        this.tipo = t;
+    }
+}
